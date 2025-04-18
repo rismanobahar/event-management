2a5c873a9812b9ca7815828052a72ad6b010de63
rismanobahar <rismanobahar@gmail.com>
2025-04-18 20:20:33 +0700

    04/18/2025

--- event-management/app/Http/Controllers/Api/EventController.php
+++ event-management/app/Http/Controllers/Api/EventController.php
@@ -14,18 +14,57 @@ class EventController extends Controller
      */
     public function index()
     {
-        $this->shouldIncludeRelation('user'); // Check if the user relationship should be included in the response
+        $query = Event::query(); // Create a query builder instance for the Event model
+        $relations = ['user', 'attendees', 'attendees.user']; // Define the relationships to be eager loaded
+
+        foreach ($relations as $relation){
+            $query->when( 
+                $this->shouldIncludeRelation($relation), // Check if the relationship should be included
+                fn($q) => $q->with($relation) // Eager load the relationship
+            );
+        }
+
+        /* the following code check if the user relationship should be included in the response.
+         this code is only used to test the function */
+        // $this->shouldIncludeRelation('user');
+
         // return Event::all(); // Return all events from the database
+        /* updated to the following line */
         // return EventResource::collection(Event::all()); // Return all events from the database
-        return EventResource::collection(Event::with('user')->paginate()); // Return all events from the database with the user relationship
+        /* updated to the following line */ 
+        return EventResource::collection(
+            // Event::with('user')->paginate()
+            /* the above line is not used because it already has a query with the necessary relationship */
+            $query->latest()->paginate() // Return all events from the database with pagination
+        ); // Return all events from the database with the user relationship
     }
 
-    // the following method is used to check if the user relationship should be included in the response
+    /* the following method is used to check if the user relationship should be included in the response. 
+    * so basically this method checks if the 'include' query parameter is present in the request and if it contains the specified relation.
+    * for example if the request is /api/events?include=user, it will return true for the user relation.
+    * if the request is /api/events?include=user,attendees it will return true for both user and attendees relations.
+    *  if the request is /api/events?include=user,attendees.attendees.user it will return true for both user, attendees, and attendees.user relations.
+    */
+    
     protected function shouldIncludeRelation(string $relation): bool
     {
         $include = request()->query('include'); // Get the 'include' query parameter from the request
 
-        dd($include); // Dump and die the 'include' query parameter
+        if (!$include) {
+            return false; // If the 'include' parameter is not present, return false
+        }
+
+        // $relations = explode(',', $include); // Split the 'include' parameter by commas
+        $relations = array_map('trim', explode(',', $include)); // Split the 'include' parameter by commas and trim whitespace if the endpoint is a string with spaces
+
+        // dd($include); // Debugging line to check the include parameter
+        // dd($relations); // Debugging line to check the relations and turn it into an array
+       
+        /* after there is a trim function to remove the spaces, we can
+           use the following line to check if the relation is in the array of
+           relations, not using dd anymore */
+        
+        return in_array($relation, $relations); // Check if the specified relation is in the array of relations
     }
 
     /**
@@ -39,7 +78,7 @@ class EventController extends Controller
                 'description' => 'nullable|string', // Event description is optional
                 'start_time' => 'required|date', // Event start time is required and must be a valid date
                 'end_time' => 'required|date|after:start_time' // Event end time is required, must be a valid date, and must be after the start time
-            ]), 
+            ]),
             'user_id' => 1 // User ID is hardcoded for now, 1 means that this will be the first user
         ]);
 
