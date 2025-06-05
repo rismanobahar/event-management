8f835116c9635912e49f4126f8696cfb3243318b
rismanobahar <rismanobahar@gmail.com>
2025-06-05 08:18:58 +0700

    05/06/2025

--- event-management/app/Console/Commands/SendEventReminders.php
+++ event-management/app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -36,8 +37,13 @@ class SendEventReminders extends Command
 
         $this->info("Found {$eventCount} {$eventLabel}."); // Display the number of events found
 
-        $events->each(fn ($event) => $event->attendees->each(
-            fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+        $events->each(
+            fn ($event) => $event->attendees->each(
+            fn ($attendee) => $attendee->user->notify(
+                new EventReminderNotification(
+                    $event // Notify the user with the event reminder notification
+                )
+            )
         )); // Notify each attendee of the event
         
         $this->info('Reminder notification sent successfully!'); // Display a success message
--- event-management/app/Notifications/EventReminderNotification.php
+++ event-management/app/Notifications/EventReminderNotification.php
@@ -51,7 +51,9 @@ class EventReminderNotification extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            'event_id' => $this->event->id,
+            'event_name' => $this->event->name,
+            'event_start_time' => $this->event->start_time
         ];
     }
 }
